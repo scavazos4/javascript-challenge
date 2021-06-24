16a61f04bf6fd231f4f5ed15d12ae75c81a1bf7c
Shawn Cavazos <scavazos4@gmail.com>
2021-06-24 17:50:29 -0500

    note

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,47 @@
-// from data.js
-var tableData = data;
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+// Console.log the weather data from data.js
+console.log(data);
+
+// Step 1:
+data.forEach(function(event) {
+  console.log(event);
+});
+
+// // // Step 2:
+// data.forEach(function() {
+//   var row = tbody.append("tr");
+// });
+
+// // Step 3: 
+// data.forEach(function(event) {
+
+//  var row = tbody.append("tr");
+
+//   Object.entries(event).forEach(function([key, value]) {
+//     console.log(key, value);
+//  });
+// });
+
+// Step 4: 
+// data.forEach(function(event) {
+//     console.log(event);
+//     var row = tbody.append("tr");
+  
+//     Object.entries(event).forEach(function([key, value]) {
+//       console.log(key, value);
+//       var cell = row.append("td");
+//     });
+//   });
+
+  // Step 5:
+data.forEach(function(event) {
+    console.log(event);
+    var row = tbody.append("tr");
+    Object.entries(event).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
